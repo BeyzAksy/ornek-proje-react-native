5f524b1bcfcd62b4d5a765d9bf3ce4d4afa152cd
Beyza Aksoy <beyzaaksoy@gmail.com>
2018-09-27 15:26:53 +0300

    Header and Liste components created.

---
+++ src/components/Liste.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Liste extends Component {
+  render() {
+    return (
+      <View style={{ marginTop: 5 }}>
+        <Text> Liste Sayfası </Text>
+      </View>
+    );
+  }
+}
+
+export default Liste;
