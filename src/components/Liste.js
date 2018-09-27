376be343d8db9c31265c91663c3d9ffcc7bbd140
Beyza Aksoy <beyzaaksoy@gmail.com>
2018-09-28 01:56:50 +0300

    Gelen datalar render edildi.

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class Liste extends Component {
+  state = { data: [] };
+  componentWillMount() {
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ data: response.data }));
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  renderData() {
+    return this.state.data.map(responseData =>
+    <Text> {responseData.title} </Text>
+  );
+  }
   render() {
+    console.log('gelen data:' + this.state);
     return (
       <View style={{ marginTop: 5 }}>
-        <Text> Liste Sayfası </Text>
+        {this.renderData()}
       </View>
     );
   }
